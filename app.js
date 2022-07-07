dcc086751e2e15781fa95d6f2283c5f2b41a684a
unknown <semtehrani@yahoo.com>
2022-07-07 09:07:01 -0700

    Replaced with correct files

--- app.js
+++ app.js
@@ -1,159 +1,186 @@
-// <<<<<<<<<<<<<<<<< DATA SET <<<<<<<<<<<<<<<<<
-let dishes = [
-  {
-    id: 1,
-    name: "Pizza",
-    cuisine: "Italian",
-    servings: 8,
-    ingredients: ["tomato", "cheese"],
-  },
-  {
-    id: 2,
-    name: "Spaghetti",
-    cuisine: "Italian",
-    servings: 2,
-    ingredients: ["tomato", "cheese"],
-  },
-  {
-    id: 3,
-    name: "Ravioli",
-    cuisine: "Italian",
-    servings: 2,
-    ingredients: ["tomato", "cheese"],
-  },
-  {
-    id: 4,
-    name: "Enchiladas",
-    cuisine: "Mexican",
-    servings: 6,
-    ingredients: ["tomato", "cheese"],
-  },
-  {
-    id: 5,
-    name: "Tacos",
-    cuisine: "Mexican",
-    servings: 4,
-    ingredients: ["tomato", "cheese"],
-  },
-  {
-    id: 6,
-    name: "Burrito Supreme",
-    cuisine: "Mexican",
-    servings: 1,
-    ingredients: ["tomato", "cheese"],
-  },
-  {
-    id: 7,
-    name: "Elote",
-    cuisine: "Mexican",
-    servings: 4,
-    ingredients: ["corn", "cheese"],
-  },
-  {
-    id: 8,
-    name: "Crepes",
-    cuisine: "French",
-    servings: 1,
-    ingredients: ["flour", "sugar"],
-  },
-  {
-    id: 9,
-    name: "Corned Beef & Cabbage",
-    cuisine: "Irish",
-    servings: 10,
-    ingredients: ["beef", "cabbage"],
-  },
-  {
-    id: 10,
-    name: "Beef Stew",
-    cuisine: "Irish",
-    servings: 8,
-    ingredients: ["beef", "tomato"],
-  },
-  {
-    id: 11,
-    name: "Lasagna",
-    cuisine: "Vegetarian",
-    servings: 8,
-    ingredients: ["tomato", "cheese"],
-  },
-  {
-    id: 12,
-    name: "Falafel",
-    cuisine: "Vegetarian",
-    servings: 1,
-    ingredients: ["chickpea", "parsley"],
-  },
-  {
-    id: 13,
-    name: "Chili",
-    cuisine: "Vegetarian",
-    servings: 13,
-    ingredients: ["tomato", "chickpea"],
-  },
-  {
-    id: 14,
-    name: "Goulash",
-    cuisine: "Hungarian",
-    servings: 15,
-    ingredients: ["beef", "tomato"],
-  },
-];
+/*
+    Author: devCodeCamp
+    Description: Most Wanted Starter Code
+*/
+//////////////////////////////////////////* Beginning Of Starter Code *//////////////////////////////////////////
 
-// <<<<<<<<<<<<<<<<< EXAMPLE FUNCTION <<<<<<<<<<<<<<<<<
+"use strict";
+//? Utilize the hotkey to hide block level comment documentation
+////* Mac: Press "CMD"+"K" and then "CMD"+"/"
+////* PC: Press "CTRL"+"K" and then "CTRL"+"/"
 
-function findMexicanFood() {
-  let results = dishes.filter(function (el) {
-    if (el.cuisine === "Mexican") {
-      return true;
-    } else {
-      return false;
+/**
+ * This is the main logic function being called in index.html.
+ * It operates as the entry point for our entire application and allows
+ * our user to decide whether to search by name or by traits.
+ * @param {Array} people        A collection of person objects.
+ */
+function app(people) {
+    // promptFor() is a custom function defined below that helps us prompt and validate input more easily
+    // Note that we are chaining the .toLowerCase() immediately after the promptFor returns its value
+    let searchType = promptFor(
+        "Do you know the name of the person you are looking for? Enter 'yes' or 'no'",
+        yesNo
+    ).toLowerCase();
+    let searchResults;
+    // Routes our application based on the user's input
+    switch (searchType) {
+        case "yes":
+            searchResults = searchByName(people);
+            break;
+        case "no":
+            //! TODO #4: Declare a searchByTraits (multiple traits) function //////////////////////////////////////////
+                //! TODO #4a: Provide option to search for single or multiple //////////////////////////////////////////
+            searchResults = searchByTraits(people);
+            break;
+        default:
+            // Re-initializes the app() if neither case was hit above. This is an instance of recursion.
+            app(people);
+            break;
     }
-  });
-
-  //Debug tip: Place a Breakpoint on the return AFTER the filter, then hover over "results" to see results of filter without having to step through the whole thing!
-
-  return results;
+    // Calls the mainMenu() only AFTER we find the SINGLE PERSON
+    mainMenu(searchResults, people);
 }
+// End of app()
 
-let mexicanFood = findMexicanFood();
-console.log("Mexican Foods: ", mexicanFood);
+/**
+ * After finding a single person, we pass in the entire person-object that we found,
+ * as well as the entire original dataset of people. We need people in order to find
+ * descendants and other information that the user may want.
+ * @param {Object[]} person     A singular object inside of an array.
+ * @param {Array} people        A collection of person objects.
+ * @returns {String}            The valid string input retrieved from the user.
+ */
+function mainMenu(person, people) {
+    // A check to verify a person was found via searchByName() or searchByTrait()
+    if (!person[0]) {
+        alert("Could not find that individual.");
+        // Restarts app() from the very beginning
+        return app(people);
+    }
+    let displayOption = prompt(
+        `Found ${person[0].firstName} ${person[0].lastName}. Do you want to know their 'info', 'family', or 'descendants'?\nType the option you want or type 'restart' or 'quit'.`
+    );
+    // Routes our application based on the user's input
+    switch (displayOption) {
+        case "info":
+            //! TODO #1: Utilize the displayPerson function //////////////////////////////////////////
+            // HINT: Look for a person-object stringifier utility function to help
+            let personInfo = displayPerson(person[0]);
+            alert(personInfo);
+            break;
+        case "family":
+            //! TODO #2: Declare a findPersonFamily function //////////////////////////////////////////
+            // HINT: Look for a people-collection stringifier utility function to help
+            let personFamily = findPersonFamily(person[0], people);
+            alert(personFamily);
+            break;
+        case "descendants":
+            //! TODO #3: Declare a findPersonDescendants function //////////////////////////////////////////
+            // HINT: Review recursion lecture + demo for bonus user story
+            let personDescendants = findPersonDescendants(person[0], people);
+            alert(personDescendants);
+            break;
+        case "restart":
+            // Restart app() from the very beginning
+            app(people);
+            break;
+        case "quit":
+            // Stop application execution
+            return;
+        default:
+            // Prompt user again. Another instance of recursion
+            return mainMenu(person, people);
+    }
+}
+// End of mainMenu()
 
-// <<<<<<<<<<<<<<<<< PROBLEMS <<<<<<<<<<<<<<<<<
+/**
+ * This function is used when searching the people collection by
+ * a person-object's firstName and lastName properties.
+ * @param {Array} people        A collection of person objects.
+ * @returns {Array}             An array containing the person-object (or empty array if no match)
+ */
+function searchByName(people) {
+    let firstName = promptFor("What is the person's first name?", chars);
+    let lastName = promptFor("What is the person's last name?", chars);
 
-//1. Create a function that will return all dishes with the cuisine type of "vegetarian"
-//Filter
+    // The foundPerson value will be of type Array. Recall that .filter() ALWAYS returns an array.
+    let foundPerson = people.filter(function (person) {
+        if (person.firstName === firstName && person.lastName === lastName) {
+            return true;
+        }
+    });
+    return foundPerson;
+}
+// End of searchByName()
 
-//2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
-//Filter
+/**
+ * This function will be useful for STRINGIFYING a collection of person-objects
+ * first and last name properties in order to easily send the information
+ * to the user in the form of an alert().
+ * @param {Array} people        A collection of person objects.
+ */
+function displayPeople(people) {
+    alert(
+        people
+            .map(function (person) {
+                return `${person.firstName} ${person.lastName}`;
+            })
+            .join("\n")
+    );
+}
+// End of displayPeople()
 
-//3. Create a function that will return only dishes whose serving id number matches their serving count.
-//Filter
+/**
+ * This function will be useful for STRINGIFYING a person-object's properties
+ * in order to easily send the information to the user in the form of an alert().
+ * @param {Object} person       A singular object.
+ */
+function displayPerson(person) {
+    let personInfo = `First Name: ${person.firstName}\n`;
+    personInfo += `Last Name: ${person.lastName}\n`;
+    //! TODO #1a: finish getting the rest of the information to display //////////////////////////////////////////
+    alert(personInfo);
+}
+// End of displayPerson()
 
-//4. Create a function that will return only dishes whose serving count is even.
-//Filter
+/**
+ * This function's purpose is twofold:
+ * First, to generate a prompt with the value passed in to the question parameter.
+ * Second, to ensure the user input response has been validated.
+ * @param {String} question     A string that will be passed into prompt().
+ * @param {Function} valid      A callback function used to validate basic user input.
+ * @returns {String}            The valid string input retrieved from the user.
+ */
+function promptFor(question, valid) {
+    do {
+        var response = prompt(question).trim();
+    } while (!response || !valid(response));
+    return response;
+}
+// End of promptFor()
 
-//5. Create a function that will return dishes whose ingredients array is "tomato" and "cheese".
-//Filter
+/**
+ * This helper function checks to see if the value passed into input is a "yes" or "no."
+ * @param {String} input        A string that will be normalized via .toLowerCase().
+ * @returns {Boolean}           The result of our condition evaluation.
+ */
+function yesNo(input) {
+    return input.toLowerCase() === "yes" || input.toLowerCase() === "no";
+}
+// End of yesNo()
 
-//6a. Create a function that will return an array of only the names of the cuisine types. Ie ['Italian', 'Mexican', ...]
-//Map
+/**
+ * This helper function operates as a default callback for promptFor's validation.
+ * Feel free to modify this to suit your needs.
+ * @param {String} input        A string.
+ * @returns {Boolean}           Default validation -- no logic yet.
+ */
+function chars(input) {
+    return true; // Default validation only
+}
+// End of chars()
 
-// BONUS: (come back to this after finishing all)
-//6b. Use the filter method to eliminate duplicates, leaving only distinct values in the array
-
-//7. Create a function that will append the cuisine type to the start of the dish's name. Ie, "Italian Pizza"
-//Map
-
-//8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
-//Map, Filter
-
-// <<<<<<<<<<<<<<<<< BONUS <<<<<<<<<<<<<<<<<
-//9. Create a function that will return dishes whose ingredients array INCLUDES "chickpea".
-//Hint: You do not want to check the array's indexes to find out what the array INCLUDES.
-//Filter
-
-//10. Create a function that will return the total serving count of all dishes.
-//Must use Reduce, not a loop.
-
-//11. Create a function that will return an array of any objects that do not share a cuisine type with any other objects.
+//////////////////////////////////////////* End Of Starter Code *//////////////////////////////////////////
+// Any additional functions can be written below this line 👇. Happy Coding! 😁
